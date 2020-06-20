3b9cb0ae4cb3a24f9ac78762f64165465f5ed534
Antriksh Devgan <antrikshdevgan@gmail.com>
2020-06-20 14:41:49 +0000

    first commit

---
+++ user_data_kubernetes.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt-get install docker.io -y
+sudo groupadd docker
+sudo usermod -aG docker ${USER}
+sudo chmod 666 /var/run/docker.sock
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+sudo apt-get install curl
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+sudo apt-get update
+sudo apt-get install kubeadm kubelet kubectl -y
+sudo apt-mark hold kubeadm kubelet kubectl
+sudo swapoff -a
+
+#sudo apt-get install nginx -y
+#sudo wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+#sudo echo "deb https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list
+#sudo apt-get update
+#sudo apt install -y jenkins
+#sudo systemctl enable jenkins
+
+sudo apt-get install maven -y
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre
+sudo apt-get install openjdk-8-jdk -y
+
